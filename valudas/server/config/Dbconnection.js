b63ed53d27a3c83e51fa8c1d0cedd0ac6c8e9513
Hassan-Y-Sipra <hassansipra42135@gmail.com>
2025-04-14 11:54:28 +0530

    Update Dbconnection.js

--- valudas/server/config/Dbconnection.js
+++ valudas/server/config/Dbconnection.js
@@ -2,10 +2,11 @@ const mysql=require("mysql");
 
 
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"valudas"
+     host: 'trolley.proxy.rlwy.net',  // Railway MySQL host
+  user: 'root',                    // Username from Railway (typically root)
+  password: 'gFLLlCQVHsqnHJNLBMoggHohVnUJzbiC',  // Password from Railway
+  database: 'railway',             // Database name from Railway
+  port: 27708                      // Railway MySQL port
 });
 
 module.exports=db;
